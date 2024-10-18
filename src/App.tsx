e328dbde12f1b9c1a1a47a1ca5556cf3f0926322
Aboubakar Bamba <aboubakarbamba716@gmail.com>
2024-10-18 18:41:55 +0200

    ajout du projet avec les modifications pour le responsvie

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import LandingPage from "./components/LandingPage"
 import Home from "./components/Home"
 import Competence from "./components/Compétences"
 import Project from "./components/Project"
-import APP from "./components/APP"
 import Footer from "./components/Footer"
 import Copyright from "./components/Copyright"
 
@@ -20,7 +19,6 @@ function App() {
         <Route path="/landingpage" element={<LandingPage />}/>
         <Route path="/competence" element={<Competence />}/>
         <Route  path="/project" element={<Project />}/>
-        <Route  path="/app" element={<APP />}/>
         <Route  path="/footer" element={<Footer />}/>
         <Route  path="/copyright" element={<Copyright />}/>
       </Routes>
